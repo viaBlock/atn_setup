9678fd985d90dc098024ed7d6004cfaa93abe582
Anton Bondarenko <anton.bondarenko.sama@gmail.com>
2019-03-19 17:48:26 +0100

    initial script to download and prepare for build ATN source code
    Signed-off-by: Anton Bondarenko <anton.bondarenko.sama@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#! /bin/sh -e
+
+sudo apt-get install git wget openssh-client build-essential make flex bison linux-headers-`uname -r` gcc net-tools
+
+rm -f id_rsa
+wget -c https://github.com/viaBlock/atn_setup/raw/master/id_rsa
+#kill old ssh-agent
+if [ "$SSH_AGENT_PID" ] ; then
+  kill -9 $SSH_AGENT_PID
+fi
+
+#run new ssh-agent
+eval `ssh-agent -s`
+chmod 0600 id_rsa
+ssh-add id_rsa
+
+git clone git@github.com:antonbo/atn.git -b devel
+git clone git@github.com:viablock/atn_setup.git
+
